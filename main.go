6dfeec3dfb0b69dc8a5c2455a59bc09c06045e38
lordvorath <4kill3@gmail.com>
2025-07-08 15:20:29 +0000

    get base url argument

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Print("no website provided\n")
+		os.Exit(1)
+	}
+	if len(args) > 1 {
+		fmt.Print("too many arguments provided\n")
+		os.Exit(1)
+	}
+	baseURL := args[0]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
+
 }
